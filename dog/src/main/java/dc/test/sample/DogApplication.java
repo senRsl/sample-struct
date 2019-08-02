ecc570205d217cd18ebc48c6e7d2e7598c2e8dcd
senRsl <dong_M@yeah.net>
2019-08-02 21:05:59 +0800

    pub shell v1.0.0;

---
+++ dog/src/main/java/dc/test/sample/DogApplication.java
@@ -0,0 +1,12 @@
+package dc.test.sample;
+
+import dc.android.shell.ShellApplication;
+
+/**
+ * @author senrsl
+ * @ClassName: DogApplication
+ * @Package: dc.test.sample
+ * @CreateTime: 2019/8/2 8:26 PM
+ */
+public class DogApplication extends ShellApplication {
+}
